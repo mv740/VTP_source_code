3b48d4916cadeb0cf8bb853676f5ff7695edcf0d
Michal Wozniak <michalwozniak@live.ca>
2019-02-27 00:00:33 -0500

    replace deprecated , better compatibility for linux

--- include/stdafx.h
+++ include/stdafx.h
@@ -4,13 +4,8 @@
 #include <fstream>
 #include <iomanip>
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <cstdio>
-
 #include <algorithm>
 #include <math.h>
-#include <cmath>
 
 #include <vector>
 #include <list>
